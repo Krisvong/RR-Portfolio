e56e9167c8f804eb64a2d7f477b67653796f73e2
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-04-08 12:29:33 -0400

    added Logo to Home component

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,15 @@
+import LogoK from '../../../assets/images/logo-k.png';
+import './index.scss';
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+     
+        <img className="solid-logo" src={LogoK} alt="Developer" />
+     
+     
+    </div>
+  );
+};
+
+export default Logo;
